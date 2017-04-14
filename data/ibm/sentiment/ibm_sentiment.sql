c2467a184a290a128fe46c9c6f38789c4e658628
Lucas Donaldson <l.donaldson1995@yahoo.com>
2017-04-13 22:23:33 -0400

    weekly average sentiment for all companies added

--- data/ibm/sentiment/ibm_sentiment.sql
+++ data/ibm/sentiment/ibm_sentiment.sql
@@ -1,1 +1,6 @@
 SELECT v2tone, date FROM[gdelt-bq:gdeltv2.gkg] WHERE (organizations like '%ibm%') AND documentidentifier like '%ibm%' ;
+
+SELECT v2tone, date
+FROM [gdelt-bq:gdeltv2.gkg] 
+WHERE (organizations like '%ibm%' or organizations like '%international business machines%')
+AND (documentidentifier like '%ibm%' or documentidentifier like '%international-business-machines%'); 
